6647143f66f9f6d2dfff4799ff5db4547f22fb67
Jaewon Choi <qwebnm7788@naver.com>
2019-08-11 00:46:04 +0900

    Add title bar (#5)

--- src/App/App.js
+++ src/App/App.js
@@ -11,6 +11,8 @@ import Agenda from '../Agenda/Agenda.js';
 
 import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
+import { Typography, Box, Avatar } from '@material-ui/core';
+import Agenda from '../Agenda/Agenda';
 
 let theme = createMuiTheme();
 theme = responsiveFontSizes(theme);
@@ -20,6 +22,7 @@ class App extends React.Component {
     state = {
         user: "yegeeee"
     }
+
     _getContent = () => {
 
         const tasks = dm.default.getUserTask(this.state.user);
@@ -29,39 +32,51 @@ class App extends React.Component {
         console.log(tasks[1]);
 
         return (
-            <Grid container className="gridContainer">
-                <Grid item className="dock">
-                    <TempDrawer />
-                </Grid>
-                <Grid item className="mainSection">
-                    <ThemeProvider theme={theme}>
-                        <Typography variant="h4">Main Section</Typography>
-                    </ThemeProvider>
-                    <Grid container>
-                        <Grid item xs={4}>
-                            <Agenda title={"TODO"} number={2}></Agenda>
-                        </Grid>
-                        <Grid item xs={4}>
-                            <Agenda title={"DOING"} number={3}></Agenda>
-                        </Grid>
-                        <Grid item xs={4}>
-                            <Agenda title={"DONE"} number={3}></Agenda>
-                        </Grid>
-                    </Grid>
-                    
-                    {JSON.stringify(tasks)}
-                </Grid>
-                <Grid item className="calendarSection">
-                    <ThemeProvider theme={theme}>
-                        <Typography variant="h4">
-                            Calendar
+            <div className="gridContainer">
+                <Grid container className="header">
+                    <Grid item><Avatar alt="Jaewon" src={process.env.PUBLIC_URL + '/bells.png'}/></Grid>
+                    <Grid item>
+                        <Typography>
+                            <Box fontWeight="fontWeightBold" fontSize={32} fontStyle="italic" fontFamily="Arial" letterSpacing={3}>
+                                D-Ding~
+                            </Box>
                         </Typography>
-                    </ThemeProvider>
-                    <CalendarSection>
-                        
-                    </CalendarSection>
+                    </Grid>
                 </Grid>
-            </Grid>
+                <Grid container className="gridContainer">
+                    <Grid item className="dock">
+                        <TempDrawer />
+                    </Grid>
+                    <Grid item className="mainSection">
+                        <ThemeProvider theme={theme}>
+                            <Typography variant="h4">Main Section</Typography>
+                        </ThemeProvider>
+                        <Grid container>
+                            <Grid item xs={4}>
+                                <Agenda title={"TODO"} number={2}></Agenda>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <Agenda title={"DOING"} number={3}></Agenda>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <Agenda title={"DONE"} number={3}></Agenda>
+                            </Grid>
+                        </Grid>
+                        
+                        {JSON.stringify(tasks)}
+                        {teams}
+                    </Grid>
+                    <Grid item className="calendarSection">
+                        <ThemeProvider theme={theme}>
+                            <Typography variant="h4">
+                                Calendar
+                            </Typography>
+                        </ThemeProvider>
+                        <CalendarSection>
+                        </CalendarSection>
+                    </Grid>
+                </Grid>
+            </div>
         )
     }
 
