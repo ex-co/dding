56b219bb55fd603c6dc067f6853f690c40f1157c
yegeeee <kangyeaji@naver.com>
2019-08-18 14:17:23 +0900

    header section modification

--- src/App/HeaderSection.js
+++ src/App/HeaderSection.js
@@ -1,25 +1,26 @@
 import React from 'react'
-import Grid from '@material-ui/core/Grid'
-import { Typography, Box, Avatar } from '@material-ui/core'
+import BackGround from './DDing_background.png'
 
 class HeaderSection extends React.Component {
 
-    headerStyle={
-        backgroundColor: "#fcba03"
-    }
+    styles = {
+        headerStyle:{
+            backgroundImage: `url(${BackGround})`,
+            height:'45px',
+            display: 'flex', 
+            justifyContent: 'center',
+            alignItems: 'center'
+        },
+        canvas: {
+            height:'45px'
+        }
+      };
 
     render() {
         return (
-            <Grid container style={this.headerStyle}>
-                <Grid item><Avatar alt="Jaewon" src={process.env.PUBLIC_URL + '/bells.png'} /></Grid>
-                <Grid item>
-                    <Typography>
-                        <Box fontWeight="fontWeightBold" fontSize={32} fontStyle="italic" fontFamily="Arial" letterSpacing={3}>
-                            D-Ding~
-                            </Box>
-                    </Typography>
-                </Grid>
-            </Grid>
+            <div style={this.styles.headerStyle}>
+                <img alt="Jaewon" style = {this.styles.canvas} src={process.env.PUBLIC_URL + '/DDing_Icon3.png'} />
+            </div>
         )
     }
 }
