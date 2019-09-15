032b12f289d57d3de770a58785b0ee8bc8023e3b
yegeeee <kangyeaji@naver.com>
2019-09-16 06:43:51 +0900

    add database

---
+++ backend/db.js
@@ -0,0 +1,19 @@
+var mysql      = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'dnr930309',
+  database : 'DDing'
+});
+
+connection.connect();
+  
+connection.query('SELECT * FROM test_db', function (error, results, fields) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+});
+
+connection.end();
